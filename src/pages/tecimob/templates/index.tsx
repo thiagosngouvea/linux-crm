142207aaf72d7303bd3d165ac278f55b5eea0d7b
thiagosngouvea <thiagosngouvea@gmail.com>
2025-08-13 23:56:25 -0300

    refactor: :recycle: remove bathroom room details from template and enhance area formatting

--- src/pages/tecimob/templates/index.tsx
+++ src/pages/tecimob/templates/index.tsx
@@ -464,12 +464,6 @@ function Templates({ token }: { token: string }) {
             setImovel(res.data.data);
             
             const fullRooms = {
-                bathroom: {
-                    title_formated: res?.data?.data?.rooms?.bathroom?.title_formated,
-                    icon: <FaBath size={64}/>,
-                    priority: 6,
-                    value: res?.data?.data?.rooms?.bathroom?.value
-                },
                 bedroom: {
                     title_formated: `${res?.data?.data?.rooms?.bedroom?.value} ${res?.data?.data?.rooms?.bedroom?.value > 1 ? ' QUARTOS' : ' QUARTO'} `,
                     icon: <IoIosBed size={64} />,
@@ -1086,7 +1080,7 @@ function Templates({ token }: { token: string }) {
                                       {value.icon}
                                     </span>
                                   )}
-                                  <span style={{ marginTop: isQuartos ? 0 : shouldReplaceHA ? 50 : 30, marginLeft: shouldReplaceHA ? 10 : 0, fontFamily: "'Roboto', sans-serif", fontWeight: "bold", fontSize: shouldReplaceHA ? 48 : 32, textShadow: "0 2px 4px rgba(0,0,0,0.5)" }}>{key.includes('area') ? '': '0'}{shouldReplaceHA ? value?.title_formated.toUpperCase().replace('HA', ' HECTARES') : value?.title_formated.toUpperCase()}</span>
+                                  <span style={{ marginTop: isQuartos ? 0 : shouldReplaceHA ? 50 : 30, marginLeft: shouldReplaceHA ? 10 : 0, fontFamily: "'Roboto', sans-serif", fontWeight: "bold", fontSize: shouldReplaceHA ? 48 : 32, textShadow: "0 2px 4px rgba(0,0,0,0.5)" }}>{key.includes('area') ? '': '0'}{shouldReplaceHA ? value?.title_formated.toUpperCase().replace('HA', ' HECTARES').replace('M²', ' M²') : value?.title_formated.toUpperCase()}</span>
                                 </span>
                                 {isQuartos && suite?.title_formated && (
                                   <span style={{ marginTop: -60, color: "#fff", marginLeft: 55, textShadow: "0 2px 4px rgba(0,0,0,0.5)" }}>(0{suite.title_formated.toUpperCase()})</span>
